6f5c6dad65b6367f35af989b3edb69095cf7bb10
MonFlix <comptepourfrze@gmail.com>
2026-05-08 02:04:44 +0000

    fix discord image url

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
-});
-
 client.on('messageCreate', (message) => {
     if (message.author.bot) return;
 
     if (message.attachments.size > 0) {
-        const img = message.attachments.first();
-        message.reply(`Lien de ton image : ${img.url}`);
+        const attachment = message.attachments.first();
+
+        const url = attachment.url;
+
+        message.reply({
+            content: `🔗 Lien direct Discord de l'image :\n${url}`
+        });
     }
 });
-
-client.login(process.env.TOKEN);
