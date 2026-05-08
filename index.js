82dec85101f23c41eda9c152d09a4f1b4ea0ef89
MonFlix <comptepourfrze@gmail.com>
2026-05-08 02:07:33 +0000

    fix client error

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Bot connecté en tant que ${client.user.tag}`);
+});
+
 client.on('messageCreate', (message) => {
     if (message.author.bot) return;
 
     if (message.attachments.size > 0) {
         const attachment = message.attachments.first();
 
-        const url = attachment.url;
-
-        message.reply({
-            content: `🔗 Lien direct Discord de l'image :\n${url}`
-        });
+        message.reply(`🔗 Lien Discord : ${attachment.url}`);
     }
 });
+
+client.login(process.env.TOKEN);
